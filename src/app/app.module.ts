17de0f8fbf2af32e74a566643608e3fa7957e682
afolabiopakunle <opakunleafolabi@gmail.com>
2023-05-24 15:42:16 +0100

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { CounterButtonsComponent } from './counter-buttons/counter-buttons.component';
+import { CounterOutputComponent } from './counter-output/counter-output.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, CounterComponent, CounterButtonsComponent, CounterOutputComponent,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
